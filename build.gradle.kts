765768a59d4f51515caca67191d84cf9b4f4141b
Alex Leung <yattaro@gmail.com>
2020-05-02 17:03:46 -0400

    Add create/read/update functionality.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "ca.alexleung"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.javalin:javalin:3.8.0")
+    implementation("org.slf4j:slf4j-simple:1.7.28")
+    implementation("org.jetbrains.exposed", "exposed-core", "0.23.1")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.23.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.23.1")
+    implementation("org.postgresql:postgresql:42.2.9")
+    implementation("com.google.code.gson:gson:2.8.6")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/ca/alexleung/skratchpad/app.kt
@@ -0,0 +1,78 @@
+package ca.alexleung.skratchpad
+
+import io.javalin.Javalin
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.lang.NumberFormatException
+import com.google.gson.Gson
+
+object NotesTable : IntIdTable() {
+    val body: Column<String> = varchar("body", 255)
+}
+
+data class ErrorData(
+    var error: String
+)
+
+data class NoteData(
+    var id: Int?,
+    var body: String
+)
+
+fun ResultRow.toNoteData() = NoteData(this[NotesTable.id].value, this[NotesTable.body])
+
+fun main(args: Array<String>) {
+    val db = Database.connect(
+        "jdbc:postgresql://localhost:5432/skratchpad",
+        driver = "org.postgresql.Driver",
+        user = "postgres", password = "postgres"
+    )
+
+    transaction(db) {
+        SchemaUtils.create(NotesTable)
+    }
+
+    val app = Javalin.create().start()
+    app.get("/notes/:id", fun(ctx) {
+        val gson = Gson()
+        var id: Int
+        try {
+            id = ctx.pathParam("id").toInt()
+        } catch (nfe: NumberFormatException) {
+            ctx.result("Invalid id")
+            return
+        }
+        val noteData = transaction(db) {
+            NotesTable.select { NotesTable.id eq id }.map {
+                it.toNoteData()
+            }
+        }
+        if (noteData.isEmpty()) {
+            ctx.result(gson.toJson(ErrorData("Not found")))
+        } else {
+            ctx.result(gson.toJson(noteData[0]))
+        }
+    })
+
+    app.post("/notes", fun(ctx) {
+        val gson = Gson()
+        val note = gson.fromJson(ctx.body(), NoteData::class.java)
+        transaction(db) {
+            val exists = !NotesTable.select { NotesTable.id eq note.id }.empty()
+            if (exists) {
+                val updatedId = NotesTable.update({ NotesTable.id eq note.id }) {
+                    it[body] = note.body
+                }
+                println("Updated note '${updatedId}'")
+            } else {
+                note.id = NotesTable.insertAndGetId {
+                    it[body] = note.body
+                }.value
+                println("Inserted note '${note.id}'")
+            }
+        }
+        ctx.result(gson.toJson(note))
+    })
+}
+
